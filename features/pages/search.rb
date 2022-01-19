452a1111c3eebad904a09f79b0d0c60d8836686a
Vitor Martinez <vtrmartinez@gmail.com>
2022-01-19 18:32:10 -0300

    Finalizando primeiro cenário

--- features/pages/search.rb
+++ features/pages/search.rb
@@ -1,8 +1,22 @@
 class Search
 	include Capybara::DSL
+    include RSpec::Matchers
 
 	def search_content(content_name)
-        fill_in "search_query_top", :with => content_name
+        @content_name = content_name
+        fill_in "search_query_top", with: @content_name
         find_button('submit_search').click
 	end
+
+    def click_on_first_product(content_name)
+        find('ul.product_list.grid.row').first(:link, content_name).click
+    end
+
+    def verify_product_details(content_name)
+        if expect(page.has_css?('.primary_block.row', wait: 5))
+            expect(page.assert_selector('.price')).to eq true
+            expect(page.assert_text(content_name)).to eq true
+            expect(page.has_no_field?('.box-cart-bottom')).to eq true
+        end
+    end
 end
--- features/step_definitions/home_steps.rb
+++ features/step_definitions/home_steps.rb
@@ -3,13 +3,14 @@ Dado("que eu esteja na home do site AutomationPratice") do
 end
   
 Quando("eu buscar pelo conteúdo {string}") do |content_name|
-    Search.new.search_content(content_name)
+    @content_name = content_name
+    Search.new.search_content(@content_name)
 end
 
 Quando("abrir o primeiro produto") do
-    pending # Write code here that turns the phrase above into concrete actions
+    Search.new.click_on_first_product(@content_name)
 end
 
 Então("verei os detalhes do produto") do
-    pending # Write code here that turns the phrase above into concrete actions
+    Search.new.verify_product_details(@content_name)
 end
